206595c7aa48391b9a39e647e1dced4bfb7da224
battleguard <howard.175@wright.edu>
2013-07-31 00:38:04 -0400

    Update Runecrafter.java

--- rsbot5/src/com/battleguard/scripts/f2prunecrafter/Runecrafter.java
+++ rsbot5/src/com/battleguard/scripts/f2prunecrafter/Runecrafter.java
@@ -20,28 +20,28 @@ public class Runecrafter extends PollingScript {
 	private static final int RUNE_ESSENCE_ID = 1436;
 	
 	private final Queue<Node> bankNodes = new ConcurrentLinkedQueue<>();
-    private final Queue<Node> craftNodes = new ConcurrentLinkedQueue<>();
+    	private final Queue<Node> craftNodes = new ConcurrentLinkedQueue<>();
 	
 	public Runecrafter() {
 		getExecQueue(State.START).offer(new Runnable() {
 			 
-            @Override
-            public void run() {
-            	// this will be changed later to be selected by a gui
-            	final Master master = Master.FIRE;	
-        		
-            	final Node crafting = new Craft(master, ctx);
-            	final Node enterAlter = Doors.createEnterAlterInstance(master, ctx);
-            	final Node toAlter = Walk.createAlterPathInstance(master, ctx);             		        		        		        		        		        	
-        		
-        		final Node bank = new Banking(master, ctx);
-        		final Node exitAlter = Doors.createExitAlterInstance(master, ctx);
-        		final Node toBank = Walk.createBankPathInstance(master, ctx);         		
-        		
-        		craftNodes.addAll(Arrays.asList(crafting, enterAlter, toAlter));
-        		bankNodes.addAll(Arrays.asList(bank, exitAlter, toBank));
-            }
-        });		
+	            @Override
+	            public void run() {
+	            	// this will be changed later to be selected by a gui
+	            	final Master master = Master.FIRE;	
+	        		
+	            	final Node crafting = new Craft(master, ctx);
+	            	final Node enterAlter = Doors.createEnterAlterInstance(master, ctx);
+	            	final Node toAlter = Walk.createAlterPathInstance(master, ctx);             		        		        		        		        		        	
+	        		
+	        	final Node bank = new Banking(master, ctx);
+	        	final Node exitAlter = Doors.createExitAlterInstance(master, ctx);
+	        	final Node toBank = Walk.createBankPathInstance(master, ctx);         		
+	        	
+	        	craftNodes.addAll(Arrays.asList(crafting, enterAlter, toAlter));
+	        	bankNodes.addAll(Arrays.asList(bank, exitAlter, toBank));
+	            }
+	        });		
 	}
 	
 	
